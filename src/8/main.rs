21a4b91a2cea0f665840328cad0c77850bec989c
ytyaru <ytyaru@outlook.jp>
2019-06-29 08:44:38 +0900

    追加

---
+++ src/8/main.rs
@@ -0,0 +1,21 @@
+/*
+ * Rustのライフタイム。
+ * CreatedAt: 2019-06-28
+ */
+fn main() {
+    let str1 = "AA".to_string();
+    let result;
+    {
+        let str2 = "A";
+        result = longest(str1.as_str(), str2); // error[E0597]: `str2` does not live long enough
+    }
+    println!("{}", result);
+}
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    let result = String::from("really long string");
+    result.as_str() // error[E0597]: `result` does not live long enough
+//    "A"
+//    "A".to_string();
+//    if x.len() > y.len() { x }
+//    else { y }
+}
